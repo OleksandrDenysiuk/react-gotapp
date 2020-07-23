4ffbd9079ff39843a2ff1f4f773bdd585d640aad
Oleksandr <denysiuk.oleksandr1997@gmail.com>
2020-07-23 12:14:16 +0200

    Add pages

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,9 +2,10 @@ import React, {Component} from 'react';
 import {Col, Row, Container} from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
-import CharecterPage from '../characterPage';
+import {CharacterPage, HousesPage, BooksPage, BooksItem} from '../pages';
 import ErrorMessage from '../errorMessage';
 import GotService from '../../services/gotService';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 export default class App extends Component {
 
@@ -44,23 +45,31 @@ export default class App extends Component {
         }
 
         return (
-            <> 
-                <Container>
-                    <Header />
-                </Container>
-                <Container>
-                    <Row>
-                        <Col lg={{size: 5, offset: 0}}>
-                            {randomChar}
-                            <button 
-                            className="btn btn-primary"
-                            onClick={this.onToggleChar}
-                            >Toggle char</button>
-                        </Col>
-                    </Row>
-                    <CharecterPage/>
-                </Container>
-            </>
+            <Router>
+                <div className="app"> 
+                    <Container>
+                        <Header />
+                    </Container>
+                    <Container>
+                        <Row>
+                            <Col lg={{size: 5, offset: 0}}>
+                                {randomChar}
+                                <button 
+                                className="btn btn-primary"
+                                onClick={this.onToggleChar}
+                                >Toggle char</button>
+                            </Col>
+                        </Row>
+                        <Route path='/' component={() => <h1>Welcome to GOT DB</h1>} exact/>
+                        <Route path='/characters' component={CharacterPage} />
+                        <Route path='/books' component={BooksPage} exact/>
+                        <Route path='/books/:id' render={({match}) => {
+                            const {id} = match.params;
+                        return <BooksItem bookId={id}/>}}/>
+                        <Route path='/houses' component={HousesPage} />
+                    </Container>
+                </div>
+            </Router>
         );
     }
 };
